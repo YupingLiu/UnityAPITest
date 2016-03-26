aa267c1e5a66319581e40ccde17bb847a0752387
Yuping Liu <569478719@qq.com>
2016-03-26 13:31:37 +0800

    --Utils

---
+++ UnitAPITest/Assets/Scripts/CollectionUtil.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MoreFun.Utils
+{
+    public class CollectionUtil
+    {
+        public static bool IsValidIndex(System.Collections.IList collection, int index)
+        {
+            if(null != collection &&
+                0 <= index &&
+                index < collection.Count &&
+                null != collection[index])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
