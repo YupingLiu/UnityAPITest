a5ab9be696dae76ed1e9c63efca378b6fd83f0ba
Yuping Liu <569478719@qq.com>
2016-03-29 19:59:37 +0800

    --Create obj test

---
+++ UnitAPITest/Assets/Scripts/EditorTest/ObjectBuilderScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class ObjectBuilderScript : MonoBehaviour {
+
+    public GameObject obj;
+    public Vector3 spawnPoint;
+
+    public void BuildObject()
+    {
+        Instantiate(obj, spawnPoint, Quaternion.identity);
+    }
+}
